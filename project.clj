b41e705c37fc1cbe99f2521c1b859075b4816544
Tony Vu <vu.tony@gmail.com>
2019-07-02 15:50:26 -0700

    (SERVER-2575) Configure clj-shell-utils
    After patching in the relevant history from puppetlabs/puppetserver,
    this commit adds the remaining necessary components to finish making the
    shell-utils its own library.

---
+++ project.clj
@@ -0,0 +1,40 @@
+(defproject puppetlabs/clj-shell-utils "1.0.0-SNAPSHOT"
+  :description "Clojure shell execution utilities"
+
+  :min-lein-version "2.9.0"
+
+  :parent-project {:coords [puppetlabs/clj-parent "3.0.0"]
+                   :inherit [:managed-dependencies]}
+
+  :pedantic? :abort
+
+  :test-paths ["test/unit"]
+
+  :plugins [[lein-project-version "0.1.0"]
+            [lein-parent "0.3.6"]]
+
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
+
+  :dependencies [[org.clojure/clojure]
+                 [prismatic/schema]
+                 [org.apache.commons/commons-exec]
+                 [commons-io]
+                 [org.slf4j/log4j-over-slf4j]
+                 [org.slf4j/slf4j-api]
+                 [puppetlabs/trapperkeeper]
+                 [puppetlabs/kitchensink]
+                 [puppetlabs/i18n]]
+
+
+
+  :profiles { :test { :dependencies [[puppetlabs/trapperkeeper nil :classifier "test" :scope "test"]]}}
+
+  :deploy-repositories [["releases" {:url "https://clojars.org/repo"
+                                     :username :env/clojars_jenkins_username
+                                     :password :env/clojars_jenkins_password
+                                     :sign-releases false}]
+                        ["snapshots" "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]]
+  )
+
+
